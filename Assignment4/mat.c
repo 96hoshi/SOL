b159b5d62efc15cefe6254a006c935c33e6ccb1c
96hoshi <m.locascio96@hotmail.com>
2019-04-07 13:04:02 +0200

    exercise completed

--- Assignment4/mat.c
+++ Assignment4/mat.c
@@ -9,28 +9,41 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	FILE *input = NULL;
+	FILE *output_dat = NULL;
+	FILE *output_txt = NULL;
 	int N = 0;
 	float *M1 = NULL;
 
-	input = fopen(argv[1],"r");
-	if(N == NULL){
-		fprintf(stderr, "Il file in input \"%s\" non esiste o non puo' essere aperto in lettura\n", argv[1]);
-		fclose(N);
+	N = (int)strtol(argv[1], NULL, 10);
+	M1 = (float*) malloc((N * N) * sizeof(float));
+	for(size_t i = 0; i < N; ++i)
+		for(size_t j = 0; j < N; ++j)
+			M1[i * N + j] = (i + j)/2.0;
+	
+	output_dat = fopen("output.dat", "w");
+	if(output_dat == NULL){
+		fprintf(stderr, "output_dat");
+		fclose(output_dat);
+		exit(EXIT_FAILURE);
+	}	
+
+	fwrite(M1, sizeof(*M1), N * N, output_dat);
+	fclose(output_dat);
+
+	output_txt = fopen("output.txt", "w");
+	if(output_txt == NULL){
+		fprintf(stderr, "output_txt");
+		fclose(output_txt);
+		fclose(output_dat);
 		exit(EXIT_FAILURE);
 	}
 
-	fscanf(input, "%d", &N);
-	M1 = (float*) malloc((N*N) * sizeof(float));
-	if(M1 == NULL){
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
+	for(size_t i = 0; i < N; ++i)
+		for(size_t j = 0; j < N; ++j)
+			fprintf(output_txt, "%f\n", M1[i * N + j]);
+	fclose(output_txt);
 
-	 for(size_t i = 0; i < N; ++i){
-		for(size_t j = 0;j < N; ++j)
-			M1[i* N + j] = (i+j)/2.0;
-	}
+	free(M1);
 
 	return 0;
 }
